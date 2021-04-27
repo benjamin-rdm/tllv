9c39331fc742a25c46a22f6f46d5608121029119
benjamin <benjamin.riedemann@protonmail.com>
2021-04-27 15:14:54 +0200

    Instances for Nat for Eq, Ord, Num

--- natural.hs
+++ natural.hs
@@ -7,6 +7,30 @@ module Natural where
 
 data Nat = Zero | Succ Nat
 
+instance Eq Nat where
+    (Succ n) == (Succ m) = n == m
+    Zero     == Zero     = True
+    _        == _        = False
+
+instance Ord Nat where
+    compare (Succ n) (Succ m) = compare n m
+    compare Zero     Zero     = EQ
+    compare Zero     _        = LT
+    compare _        _        = GT
+
+instance Num Nat where
+    Zero     + y = y
+    (Succ x) + y = Succ (x + y)
+    Zero     * y = Zero
+    (Succ x) * y = y + (x * y)
+    abs = id
+    fromInteger 0 = Zero
+    fromInteger n = Succ (fromInteger (n-1))
+    (Succ n) - (Succ m) = n - m
+    n        - Zero     = n
+    signum Zero = 0
+    signum _    = 1
+
 type family (x :: Nat) + (y :: Nat) where
     Zero     + y = y
     (Succ x) + y = Succ (x + y)
